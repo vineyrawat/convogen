d0163736399639786a1c447f0e9ff979a6069fe5
Vinay Rawatt <vineyrawat@yahoo.com>
2023-12-18 23:08:34 +0530

    Added Settings Page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/cupertino.dart';
-import 'package:gemini_client/widgets/developmentmode.dart';
+import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DevelopmentMode();
+    var inputController = TextEditingController();
+
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        children: [
+          TextField(
+            controller: inputController,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              label: const Text("API Key"),
+              hintText: 'Add your gemini api key here',
+              contentPadding: const EdgeInsets.all(15),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FilledButton(onPressed: () {}, child: const Text("Save"))
+        ],
+      ),
+    );
   }
 }
